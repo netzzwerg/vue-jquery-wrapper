78203802a898af44883544b2d1545e77747b42a9
Béla Varga <bva@financial.com>
2017-06-21 07:56:51 +0200

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
